0ec6fdde838abf8204d9e7070f04fd0230c26bec
Avin Sharma <sharmaavin@hotmail.com>
2019-08-31 20:16:52 -0400

    Sorting visual feedback.
    Build up for yelp search

--- src/App/App.js
+++ src/App/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import BusinessList from '../components/BusinessList/BusinessList'
 import SearchBar from '../components/SearchBar/SearchBar'
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business]
+
+function searchYelp(term, location, sortBy){
+  console.log(`Searching yelp for ${term}, at ${location}, ${sortBy}`)
+}
+
 function App() {
   return (
     <div className="App">
       <h1>famished</h1>
-      <SearchBar />
-      <BusinessList />
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses}/>
     </div>
   );
 }
