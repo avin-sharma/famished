e04c75247ca4950abc9facff9aa59d3d61ec2272
Avin Sharma <sharmaavin@hotmail.com>
2019-09-02 10:32:39 -0400

    removed cors anywhere

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -4,7 +4,7 @@ const apiKey = `${process.env.REACT_APP_API_KEY}`
 let Yelp = {
     search(term, location, sortBy){
         console.log(apiKey)
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        return fetch(`https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
         {
             headers: {
                 Authorization: `Bearer ${apiKey}`
