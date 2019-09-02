7b6291a2d59d25ddc4604a8c930bfaab6f5533fb
Avin Sharma <sharmaavin@hotmail.com>
2019-09-01 23:03:46 -0400

    Added queries from yelp, removed static data

---
+++ src/util/Yelp.js
@@ -0,0 +1,36 @@
+
+const apiKey = `${process.env.REACT_APP_API_KEY}`
+
+let Yelp = {
+    search(term, location, sortBy){
+        console.log(apiKey)
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json()
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses){
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.address,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zipCode,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.reviewCount
+                    }
+                })
+            }
+        })
+    } 
+}
+
+
+export default Yelp
